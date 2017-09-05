54a51dc448a77c47f28270b71f0172af659d259f
xudaning <xudaning@qq.com>
2017-09-06 00:39:05 +0800

    徐老师提交

---
+++ src/main/java/com/example/controller/XuTest1.java
@@ -0,0 +1,10 @@
+package com.example.controller;
+
+/**
+ * Created by Administrator on 2017/9/6 0006.
+ */
+public class XuTest1 {
+    public static void main(String[] args) {
+        System.out.println("xulaoshi_branch");
+    }
+}
