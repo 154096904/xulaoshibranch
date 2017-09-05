6b504a8d5b58cf83f348a51aa8548b9e5c7a9b45
xudaning <xudaning@qq.com>
2017-09-05 23:58:13 +0800

    第一次提交

---
+++ src/main/java/com/example/controller/JavaTest.java
@@ -0,0 +1,10 @@
+package com.example.controller;
+
+/**
+ * Created by Administrator on 2017/9/5 0005.
+ */
+public class JavaTest {
+    public static void main(String[] args) {
+        System.out.println("first java");
+    }
+}
